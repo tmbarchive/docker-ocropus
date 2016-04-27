16bd35a36ba24fa72af70a009e190ccab06d4551
Konstantin Baierer <unixprog@gmail.com>
2016-04-27 03:02:25 +0200

    Fixed path in ocrotrain.sh

--- ocrotrain.sh
+++ ocrotrain.sh
@@ -7,4 +7,4 @@ fi
 export LANG=C.utf-8
 export PYTHONIOENCODING=utf-8
 /usr/bin/python -c 'import locale; print locale.getdefaultlocale(); print u"\u0411\n"'
-/ocropus/ocropy/ocropus-rtrain -o 'model-%03d' uw3-100/*/*.png
+/ocropy/ocropus-rtrain -o 'model-%03d' uw3-100/*/*.png
